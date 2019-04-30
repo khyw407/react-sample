bf7ad3a53bc14bc44109809108fe904152e7e1a5
Kim Hyunwoo <hw.kim@gsshop.com>
2019-04-30 18:04:04 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,79 @@
-import React from 'react';
+import React, {Component} from 'react'
+import Customer from './components/Customer'
+import './App.css'
+import Table from '@material-ui/core/Table'
+import TableHead from '@material-ui/core/TableHead'
+import TableBody from '@material-ui/core/TableBody'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX : 'auto'
+  },
+  table: {
+    minWidth: 1080
+  }
+})
+
+const customers = [
+{
+  id : 1,
+  image : 'https://placeimg.com/64/64/1',
+  name : '홍길동',
+  birthday : '20190430',
+  gender : '남자',
+  job : '대학생'
+},
+{
+  id : 2,
+  image : 'https://placeimg.com/64/64/2',
+  name : '세종대왕',
+  birthday : '960508',
+  gender : '남자',
+  job : '프로그래머'
+},
+{
+  id : 3,
+  image : 'https://placeimg.com/64/64/3',
+  name : '이순신',
+  birthday : '961127',
+  gender : '남자',
+  job : '디자이너'
+}
+]
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c => {
+              return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+            })}
+          </TableBody>
+        </Table>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+/*import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,6 +84,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <p>hello world!!!!!</p>
         </p>
         <a
           className="App-link"
@@ -24,3 +100,4 @@ function App() {
 }
 
 export default App;
+*/
